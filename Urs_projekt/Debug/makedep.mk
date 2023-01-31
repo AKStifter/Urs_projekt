7409653751dbf65e6055da546e7e309f0db6437d
AKStifter <95703322+AKStifter@users.noreply.github.com>
2023-01-31 19:33:46 +0100

    Početne knjižnice
    Prvi commit sa potrebnim knjižnicama za prikaz na avr mikrokontrolerima.

---
+++ Urs_projekt/Debug/makedep.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+main.cpp
+
+UTFT\color.c
+
+UTFT\DefaultFonts.c
+
+UTFT\fastbitmap.s
+
+UTFT\fastfill.s
+
+UTFT\fastio.s
+
+UTFT\fastline.s
+
+UTFT\had.c
+
+UTFT\ssd1289.c
+
+UTFT\UTFT.cpp
+
--- Urs_projekt/main.cpp
+++ Urs_projekt/main.cpp
@@ -1,18 +1,10 @@
-/*
- * Urs_projekt.cpp
- *
- * Created: 1/31/2023 7:08:55 PM
- * Author : Korisnik
- */ 
-
 #include <avr/io.h>
 
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+	/* Replace with your application code */
+	while (1)
+	{
+	}
 }
-
